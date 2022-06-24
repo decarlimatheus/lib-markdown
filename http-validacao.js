b15808ee70353b470ea0a993dc2e1de34707482d
Matheus De Carli <matheuszdecarli@gmail.com>
2022-06-24 17:04:27 -0300

    formatação dos arquivos

--- http-validacao.js
+++ http-validacao.js
@@ -1,38 +1,36 @@
 import fetch from "node-fetch";
 
 function manejaErros(erro) {
-    throw new Error(erro.message);
+  throw new Error(erro.message);
 }
 
 async function checaStatus(arrayURLs) {
-    try {
-        const arrayStatus = await Promise
-            .all(arrayURLs
-            .map(async url => {
-            const res = await fetch(url)
-            return res.status;
-        }))
-        return arrayStatus;
-    } catch(erro) {
-        manejaErros(erro);
-    }
+  try {
+    const arrayStatus = await Promise.all(
+      arrayURLs.map(async (url) => {
+        const res = await fetch(url);
+        return res.status;
+      })
+    );
+    return arrayStatus;
+  } catch (erro) {
+    manejaErros(erro);
+  }
 }
 
 function geraArrayDeURLs(arrayLinks) {
-    return arrayLinks
-    .map(objetoLink => Object
-        .values(objetoLink).join());
+  return arrayLinks.map((objetoLink) => Object.values(objetoLink).join());
 }
 
 async function validaURLs(arrayLinks) {
-    const links = geraArrayDeURLs(arrayLinks);
-    const statusLinks= await checaStatus(links);
-    //spread operator
-    const resultados = arrayLinks.map((objeto, indice) => ({ 
-        ...objeto, 
-        status: statusLinks [indice] 
-    }));
-    return resultados;
+  const links = geraArrayDeURLs(arrayLinks);
+  const statusLinks = await checaStatus(links);
+  //spread operator
+  const resultados = arrayLinks.map((objeto, indice) => ({
+    ...objeto,
+    status: statusLinks[indice],
+  }));
+  return resultados;
 }
 
 export default validaURLs;
--- index.js
+++ index.js
@@ -1,28 +1,28 @@
-import chalk from 'chalk';
-import fs from 'fs';
+import chalk from "chalk";
+import fs from "fs";
 
 function extraiLinks(texto) {
-    const regex = /\[([^\]]*)\]\((https?:\/\/[^$#\s].[^\s]*)\)/gm;
-    const arrayResultados = [];
-    let temp;
-    while((temp = regex.exec(texto)) !== null) {
-        arrayResultados.push({ [temp[1]]: temp[2] })
-    }
-    return arrayResultados.length === 0 ? 'não há links' : arrayResultados;
+  const regex = /\[([^\]]*)\]\((https?:\/\/[^$#\s].[^\s]*)\)/gm;
+  const arrayResultados = [];
+  let temp;
+  while ((temp = regex.exec(texto)) !== null) {
+    arrayResultados.push({ [temp[1]]: temp[2] });
+  }
+  return arrayResultados.length === 0 ? "não há links" : arrayResultados;
 }
 
 function trataErro(erro) {
-    throw new Error(chalk.red(erro.code, 'não há arquivo no caminho'));
+  throw new Error(chalk.red(erro.code, "não há arquivo no caminho"));
 }
 
 async function pegaArquivo(caminhoDoArquivo) {
-    const encoding = 'utf-8';
-    try {
-         const texto = await fs.promises.readFile(caminhoDoArquivo, encoding)
+  const encoding = "utf-8";
+  try {
+    const texto = await fs.promises.readFile(caminhoDoArquivo, encoding);
     return extraiLinks(texto);
-    } catch(erro) {
-        trataErro(erro);
-    } 
+  } catch (erro) {
+    trataErro(erro);
+  }
 }
 
 export default pegaArquivo;
@@ -46,8 +46,6 @@ export default pegaArquivo;
 //         console.log(chalk.green(texto));
 //     })
 // }
-
-
 
 //----------------------------------------------------//
 
