c58161aaf2944c3cf982e9dff905749855f02003
Matheus De Carli <matheuszdecarli@gmail.com>
2022-06-24 15:18:18 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,65 @@
+import chalk from 'chalk';
+import fs from 'fs';
+
+function extraiLinks(texto) {
+    const regex = /\[([^\]]*)\]\((https?:\/\/[^$#\s].[^\s]*)\)/gm;
+    const arrayResultados = [];
+    let temp;
+    while((temp = regex.exec(texto)) !== null) {
+        arrayResultados.push({ [temp[1]]: temp[2] })
+    }
+    return arrayResultados.length === 0 ? 'não há links' : arrayResultados;
+}
+
+function trataErro(erro) {
+    throw new Error(chalk.red(erro.code, 'não há arquivo no caminho'));
+}
+
+async function pegaArquivo(caminhoDoArquivo) {
+    const encoding = 'utf-8';
+    try {
+         const texto = await fs.promises.readFile(caminhoDoArquivo, encoding)
+    return extraiLinks(texto);
+    } catch(erro) {
+        trataErro(erro);
+    } 
+}
+
+export default pegaArquivo;
+
+//---------------------------------------------/
+
+// function pegaArquivo(caminhoDoArquivo){
+//     const encoding = 'utf-8';
+//     fs.promises
+//     .readFile(caminhoDoArquivo, encoding)
+//     .then((texto) => chalk.green(console.log(texto)))
+//     .catch((erro) => trataErro(erro))
+// }
+
+// function pegaArquivo(caminhoDoArquivo) {
+//     const encoding = 'utf-8';
+//     fs.readFile(caminhoDoArquivo, encoding, (erro, texto) => {
+//         if (erro) {
+//             trataErro(erro);
+//         }
+//         console.log(chalk.green(texto));
+//     })
+// }
+
+
+
+//----------------------------------------------------//
+
+// //const chalk = require ('chalk');
+// import chalk from 'chalk';
+
+// console.log(chalk.blue('vamos começar!'));
+
+// const paragrafo = 'Texto retornado por uma função';
+
+// function texto(string) {
+//     return string;
+// }
+
+// console.log(texto(paragrafo));
---
+++ test/index.test.js
@@ -0,0 +1,5 @@
+import pegaArquivo from "../index";
+
+test('deve ser uma função', () => {
+    expect(typeof pegaArquivo).toBe('function');
+  });
